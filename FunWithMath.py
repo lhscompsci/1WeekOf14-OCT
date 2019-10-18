487c5c0fcbb5f6f137b3ee5c3f77c71a53821e62
Black, Samuel Thomas <sam.black@gatech.edu>
2019-10-18 08:56:56 -0500

    decision making and random numbers

--- FunWithMath.py
+++ FunWithMath.py
@@ -45,3 +45,54 @@ print(acc)
 #   Fibonacci number.
 # Write a function to compute the Nth Fibonacci number, where N is a parameter.
 #   You may assume that N will be greater than or equal to 3.
+
+
+# A Monte Carlo Simulation
+
+import random
+
+print(random.random())
+
+# Boolean expressions
+# >  greater than
+# >= greater than or equal to
+# <  less than
+# <= less than or equal to
+# == the same as [ equal to ]
+# != NOT equal to
+
+dogWeight = 25
+print(dogWeight != 25)
+catWeight = 15
+
+# compound Boolean operators
+# and
+# or
+# not
+
+print(not catWeight < 20)
+
+
+# Decision Making -- Selection statements
+a = 5
+b = 10
+c = 75
+
+if a <= b:
+    c = 45
+
+print(c)
+
+if a > b:
+    c = 45
+    if b > c:
+        a = 25
+    else:
+        a = -25
+else:
+    c = 1050
+    if b == a:
+        c = 25
+
+
+print(a, b, c)
